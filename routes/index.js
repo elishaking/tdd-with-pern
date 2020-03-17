33eca6e91ea3aff4339f295545c06b2e7bbe6e24
King Elisha <ek.chibueze@gmail.com>
2020-03-17 09:37:13 +0100

    updated index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  res.send("Home Page");
+// *** GET all shows *** //
+router.get("/shows", function(req, res, next) {
+  res.send("send shows back");
 });
 
 module.exports = router;
