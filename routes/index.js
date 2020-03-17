2c84f40c230f8a7b006b8bf5016a2f52e6e57736
King Elisha <ek.chibueze@gmail.com>
2020-03-17 15:00:01 +0100

    update show route

--- routes/index.js
+++ routes/index.js
@@ -41,4 +41,19 @@ router.post("/shows", (req, res, next) => {
     });
 });
 
+// *** update show *** //
+router.put("/shows/:id", (req, res, next) => {
+  queries
+    .update(req.params.id, req.body)
+    .then(() => {
+      return queries.getSingle(req.params.id);
+    })
+    .then(show => {
+      res.status(200).json(show);
+    })
+    .catch(error => {
+      next(error);
+    });
+});
+
 module.exports = router;
