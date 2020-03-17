6fce5e80e2aff961180aae85077bdaafd3f11859
King Elisha <ek.chibueze@gmail.com>
2020-03-17 15:17:35 +0100

    delete show route

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -150,26 +150,79 @@ describe("API Routes", () => {
         })
         .finally(() => done());
     });
+
+    it("should NOT update a show if the id field is part of the request", done => {
+      request(server)
+        .put("/api/v1/shows/1")
+        .send({
+          id: 20,
+          rating: 4,
+          explicit: true
+        })
+        .then(res => {
+          expect(res.status).toEqual(422);
+          console.log(res.body);
+          //   res.should.be.json; // jshint ignore:line
+          expect(typeof res.body === "object").toBe(true);
+
+          expect(res.body).toHaveProperty("error");
+          expect(res.body.error).toEqual("You cannot update the id field");
+          done();
+        })
+        .finally(() => done());
+    });
   });
 
-  it("should NOT update a show if the id field is part of the request", done => {
-    request(server)
-      .put("/api/v1/shows/1")
-      .send({
-        id: 20,
-        rating: 4,
-        explicit: true
-      })
-      .then(res => {
-        expect(res.status).toEqual(422);
-        console.log(res.body);
-        //   res.should.be.json; // jshint ignore:line
-        expect(typeof res.body === "object").toBe(true);
+  describe("DELETE /api/v1/shows/:id", () => {
+    it("should delete a show", done => {
+      request(server)
+        .delete("/api/v1/shows/1")
+        .then(res => {
+          expect(res.status).toEqual(200);
+          //   res.should.be.json; // jshint ignore:line
+          expect(typeof res.body === "object").toBe(true);
 
-        expect(res.body).toHaveProperty("error");
-        expect(res.body.error).toEqual("You cannot update the id field");
-        done();
-      })
-      .finally(() => done());
+          expect(res.body).toHaveProperty("name");
+          expect(res.body.name).toEqual("Suits");
+
+          expect(res.body).toHaveProperty("channel");
+          expect(res.body.channel).toEqual("USA Network");
+
+          expect(res.body).toHaveProperty("genre");
+          expect(res.body.genre).toEqual("Drama");
+
+          expect(res.body).toHaveProperty("rating");
+          expect(res.body.rating).toEqual(3);
+
+          expect(res.body).toHaveProperty("explicit");
+          expect(res.body.explicit).toBe(false);
+
+          request(server)
+            .get("/api/v1/shows")
+            .then(res => {
+              expect(res.status).toEqual(200);
+              //   res.should.be.json; // jshint ignore:line
+              expect(Array.isArray(res.body)).toBe(true);
+              expect(res.body.length).toEqual(3);
+
+              expect(res.body[0]).toHaveProperty("name");
+              expect(res.body[0].name).toEqual("Game of Thrones");
+
+              expect(res.body[0]).toHaveProperty("channel");
+              expect(res.body[0].channel).toEqual("HBO");
+
+              expect(res.body[0]).toHaveProperty("genre");
+              expect(res.body[0].genre).toEqual("Fantasy");
+
+              expect(res.body[0]).toHaveProperty("rating");
+              expect(res.body[0].rating).toEqual(3);
+
+              expect(res.body[0]).toHaveProperty("explicit");
+              expect(res.body[0].explicit).toBe(true);
+              done();
+            })
+            .finally(() => done());
+        });
+    });
   });
 });
